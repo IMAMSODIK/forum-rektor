aadfb7a1976d3880bc657a6c54eddebba0d5d314
IMAM SODIK <imamsodik00@gmail.com>
2026-02-10 12:28:26 +0700

    adf

--- resources/views/kit/export.blade.php
+++ resources/views/kit/export.blade.php
@@ -58,7 +58,7 @@
                 </td>
                 <td class="ttd">
                     @if($item->ttd)
-                        <img src="{{ public_path('storage/ttd/'.$item->ttd) }}">
+                        <img src="{{ asset('storage/ttd/'.$item->ttd) }}">
                     @endif
                 </td>
             </tr>
